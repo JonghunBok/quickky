394dfaa633a9918ad473951f49db3ef8faf2ffda
Jonghun Park <jonghun.park.194@gmail.com>
2020-04-22 17:01:58 +0900

    Decouple ids info from loginAPI.js file

--- src/service/loginAPI.js
+++ src/service/loginAPI.js
@@ -1,12 +1,12 @@
 import AWS from 'aws-sdk'
-
+import ids from '../assets/ids.json'
 
 export default {
   async login (uid, password) {
     try {
-      AWS.config.region = '' // Region
+      AWS.config.region = ids.region
       AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: ''
+        IdentityPoolId: ids.IdentityPoolId
       })
 
       return true
